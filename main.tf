9e209a6e5b91caf9cabf4dc800ccd05e4058dc8c
tkush48 <kushagra4288@gmail.com>
2025-12-16 14:09:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ resource "aws_subnet" "private" {
   count             = 2
   vpc_id            = aws_vpc.main.id
   cidr_block        = cidrsubnet("10.0.0.0/24", 4, count.index + 3)
-  availability_zone = element(["ap-south-1a", "ap-south-1b", count.index])
+  availability_zone = element(["ap-south-1a", "ap-south-1b"], count.index)
 
   tags ={
     Name = "private-subnet-${count.index +1}"
